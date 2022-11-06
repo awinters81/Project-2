f4ae603004af5b0e74f7cbfbd27fbe21528171f5
P1100s7 <mmanp277@gmail.com>
2022-11-05 21:50:46 -0400

    login stuff

--- public/login.js
+++ public/login.js
@@ -1,11 +1,42 @@
 async function signupFormHandler(event) {
     event.preventDefault();
     
-    const username = document.querySelector('#username-signup').ariaValueMax.trim();
-    const email = document.querySelector('#email-signup').ariaValueMax.trim();
-    const password = document.querySelector('#password-signup').ariaValueMax.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
     if(username && email && password) {
-        const
+        const response = await fetch('/views/layouts/login.handlebars', {
+            method: 'POST',
+            body: JSON.stringify({username, email, password}),
+            headers: { 'signup-form': './views/layouts/login.handlebars' }
+        });
+        if(response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+async function loginFromHandler(event) {
+    event.preventDefault();
+    
+    const username = document.querySelector('##username-signup').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if(username && password) {
+        const response = await fetch('/views/layouts/login.handlebars', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: { 'login-from': './views/layouts/login.handlebars'}
+        });
+        if(response.ok) {
+            document.location.replace('signup-form');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
+
+document.querySelector('.login').addEventListener()
