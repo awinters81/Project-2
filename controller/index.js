3e9c4c5eb5f143db3b178f4b4eb229b93bf075bf
P1100s7 <mmanp277@gmail.com>
2022-11-05 20:46:57 -0400

    login stuff

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,11 @@
 // lookup the technology that is need to create a drop down element 
 // as well as a banner that auto shifts elements
-//
+const router = require('express').Router();
+const apiRoutes = require('./api/');
+
+router.use('/api', apiRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
---
+++ public/login.js
@@ -0,0 +1,11 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+    
+    const username = document.querySelector('#username-signup').ariaValueMax.trim();
+    const email = document.querySelector('#email-signup').ariaValueMax.trim();
+    const password = document.querySelector('#password-signup').ariaValueMax.trim();
+
+    if(username && email && password) {
+        const
+    }
+}
