8977a1dc5bbd5469b2950f648d12772b2fd79ea8
awinters81 <alexandria.winters81@gmail.com>
2022-11-09 23:20:26 -0500

    testing handlebars

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -8,7 +8,7 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     console.log(req.session);
     Event.findAll({
-        attributes: [ 'id', 'event_title', 'event_description', 'event_location', 'event_date' ]
+        attributes: [ 'id', 'event_title', 'event_description', 'event_location', 'event_date', 'event_picture' ]
     }).then(dbEventData => {
         const events = dbEventData.map(event => {
             return event.get({ plain: true });
@@ -20,13 +20,30 @@ router.get('/', (req, res) => {
     });
 });
 
+// get a single event by id
+router.get('/', (req, res) => {
+    Event.findOne({
+        where: {id: req.params.id},
+        attributes: [ 'id', 'event_title', 'event_description', 'event_location', 'event_date', 'event_picture' ]
+    }).then(singleEvent => {
+        if(!singleEvent) {
+            res.status(400).json({message: 'no event found with this id'});
+            return;
+        }
+        res.json(singleEvent);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 // access login/signup page from the homepage
 router.get('/login', (req, res) => {
-    // check for a session and redirect to the homepage once the user is logged in
-    // if(req.session.loggedIn) {
-    //     res.redirect('/');
-    //     return
-    // }
+    //check for a session and redirect to the homepage once the user is logged in
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return
+    }
     res.render('login')
 })
 
